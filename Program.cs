0389c55f2a47419bd525dee64a5f1212484242d1
Nikita932-ctrl <65113631+Nikita932-ctrl@users.noreply.github.com>
2023-02-14 00:32:53 +0300

    task n1
    task n1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int firstNukmber = 8;
+int secondNukmber = 11;
+int max;
+
+if (firstNukmber < secondNukmber)
+{
+    max = secondNukmber;
+    Console.WriteLine($"Максимальное число : {max}");
+}
+else if (firstNukmber > secondNukmber) { 
+    max = firstNukmber;
+    Console.WriteLine($"Максимальное число : {max}");
+}
